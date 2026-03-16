2db94a3b43535bde939fd4c8c428138804c4532f
w-evey <evey.wx@gmail.com>
2026-03-16 23:54:02 +0800

    Update solutions.py

--- solutions.py
+++ solutions.py
@@ -1,13 +1,13 @@
-class Solution1:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
-        # Return an empty list if no solution is found
-        return []
+class Solution:
+    def twoSum(self, numbers, target):
+        seen = {}
+
+        for i, num in enumerate(numbers):
+            pairCheck = target - num
+            if pairCheck in seen:
+                return[seen[pairCheck], i]
+            seen[num] = i
+    
     
 class Solution2:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
