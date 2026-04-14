e2ffd206a49e89209d83c7cd635436ffba15c3b7
w-evey <evey.wx@gmail.com>
2026-04-14 15:45:40 +0800

    midterm

---
+++ validParentheses/e1141047v2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+        
+        for char in s:
+            if char in '([{':
+                stack.append(char)
+            else:
+                if not stack or stack[-1] != pairs[char]:
+                    return False
+                stack.pop()
+        
+        return len(stack) == 0
